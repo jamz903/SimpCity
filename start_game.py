f1d187690b15629c05243f1e66bbfba209e59888
srhysc <64086762+srhysc@users.noreply.github.com>
2022-01-05 15:20:51 +0800

    Update start_game for validation

--- start_game.py
+++ start_game.py
@@ -2,6 +2,7 @@ from generate_new_game_buildings import *
 from pick_buildings import *
 from play_menu import *
 from display_board import *
+from validate_saved_file import *
 import os
 
 # function to start a game
@@ -31,28 +32,37 @@ def start_game(saved_game = False):
         if os.path.exists(filename):
             board = []
             count = 0
-            # loops through each line in the .csv file
-            for i in open(filename):
-                i = i.strip("\n")
 
-                # checks if the current line is first line
-                # if is first line, retrieve the turn number, stored in the first line of the .csv file
-                if count == 0:
-                    turn_num = int(i)
-                    count += 1
-                    continue
-                # if not first line, split the line by ',' and append it to the board
-                row = i.split(",")
-                board.append(row)
+            if validate_saved_file(filename):
 
-            # as this is a saved game, need to remove buildings that are already built on the board (if any)
-            # loops through each space on the board and check if there is a building built
-            for i in range(len(board)):
-                for j in range(len(board[i])):
-                    # checks if space is empty space or building, "?" - empty space, not "?" means is a building
-                    if (board[i][j] != "?"):
-                        # removes it from the building pool
-                        buildings_list.remove(board[i][j])
+                # loops through each line in the .csv file
+                for i in open(filename):
+                    i = i.strip("\n")
+
+                    # checks if the current line is first line
+                    # if is first line, retrieve the turn number, stored in the first line of the .csv file
+                    if count == 0:
+                        turn_num = int(i)
+                        count += 1
+                        continue
+                    # if not first line, split the line by ',' and append it to the board
+                    row = i.split(",")
+                    board.append(row)
+
+                # as this is a saved game, need to remove buildings that are already built on the board (if any)
+                # loops through each space on the board and check if there is a building built
+                for i in range(len(board)):
+                    for j in range(len(board[i])):
+                        # checks if space is empty space or building, "?" - empty space, not "?" means is a building
+                        if (board[i][j] != "?"):
+                            # removes it from the building pool
+                            buildings_list.remove(board[i][j])
+
+            else:
+                #saved file is not valid
+                print("The saved file is invalid. Returning to main menu.\n")
+                return
+
         else:
             # saved file does not exists, start a new game for player
             print("No saved game file found! Starting new game...\n")
