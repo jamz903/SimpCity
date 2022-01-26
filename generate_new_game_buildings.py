25305ca5ea9238cd844a42134bfa1af3ad1fc603
jvvvstin <63656476+jvvvstin@users.noreply.github.com>
2022-01-27 00:30:36 +0800

    Update generate_new_game_buildings.py

--- generate_new_game_buildings.py
+++ generate_new_game_buildings.py
@@ -2,8 +2,8 @@
 # i.e.
 # buildings_list = [8 x BCH, 8 x FAC, 8 x HSE, 8 x SHP, 8 x HWY]
 # returns the shuffled building list
-def generate_new_game_buildings():
-    building_types = ["BCH", "FAC", "HSE", "SHP", "HWY"]
+def generate_new_game_buildings(building_types):
+
     buildings_list = []
     for i in building_types:
         for j in range(8):
