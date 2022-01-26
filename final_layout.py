837041e3d998be5d051755807e1a6b5930cc4655
jvvvstin <63656476+jvvvstin@users.noreply.github.com>
2022-01-27 00:43:46 +0800

    Update final_layout.py

--- final_layout.py
+++ final_layout.py
@@ -1,10 +1,13 @@
 from view_score import *
 from display_board import *
+from save_high_score import *
 
 # function to display the final layout & scores of the player
 # after Turn 16, when the player fills up the board.
 
-def final_layout(board):
+def final_layout(board, building_pool, debug = False, lb_names = []):
     display_board(None, board)
-    view_score(board)
+    user_score = view_score(board, building_pool)
+    save_high_score(board, user_score, debug=debug, lb_names=lb_names)
+    
     return
