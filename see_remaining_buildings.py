d121e74025a2cbdd20ccdc5cc5122b97f51cac76
jvvvstin <63656476+jvvvstin@users.noreply.github.com>
2022-02-05 02:20:17 +0800

    Update see_remaining_buildings.py

--- see_remaining_buildings.py
+++ see_remaining_buildings.py
@@ -2,10 +2,19 @@ from collections import Counter
 
 # function to display the remaining buildings
 # takes in the list of buildings
-def see_remaining_buildings(buildings_list):
+def see_remaining_buildings(buildings_list, board, building_pool):
     buildings_counter = Counter(buildings_list)
-    print("Building          Remaining")
-    print(8 * "-" + 10 * " " + 9 * "-")
-    for i in buildings_counter:
-        print(i + 15 * " " + str(buildings_counter[i]))
+    print("Building          Remaining", end = "")
+    yield
+    print(8 * "-" + 10 * " " + 9 * "-", end = "")
+    yield
+    for i in building_pool:
+        print(i + 15 * " " + str(buildings_counter[i]), end = "")
+        yield
+
+    if (len(board) >= len(set(buildings_list))):    
+        for i in range(len(board)-5):
+            print(end="")
+            yield
+        print()
     return
