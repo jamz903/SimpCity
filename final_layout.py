0904baec935f908c6778c0990e30ae44af60f5ff
jvvvstin <63656476+jvvvstin@users.noreply.github.com>
2022-02-05 02:21:08 +0800

    Update final_layout.py

--- final_layout.py
+++ final_layout.py
@@ -6,7 +6,31 @@ from save_high_score import *
 # after Turn 16, when the player fills up the board.
 
 def final_layout(board, building_pool, debug = False, lb_names = []):
-    display_board(None, board)
+    print("\nFinal layout of Simp City:")
+    #all possible values for the length of the board
+    alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    
+    #starting values 
+    topgrid = " "
+    topline = "   +"
+    
+    #dynamically create the length values for the top of the board
+    for i in range(len(board[0])):
+        topgrid = topgrid + "     " + alphabets[i]
+        topline = topline + '-----+'
+
+    print(topgrid)
+    print(topline)
+
+    for i in range(len(board)):
+        print("{:>3}|".format(str(i+1)), end="")
+        for j in range(len(board[i])):
+            if (board[i][j] != "?"):
+                print("{}".format(" " + board[i][j] + " |"), end="")
+            else:
+                print("{}".format("     |"), end="")
+        print("\n" + topline)
+
     user_score = view_score(board, building_pool)
     save_high_score(board, user_score, debug=debug, lb_names=lb_names)
     
